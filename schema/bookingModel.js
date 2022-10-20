0918d840a3ea7328891a1a856322be50b9a4693b
Mushthak <100484745+MushthakVP@users.noreply.github.com>
2022-10-20 09:00:44 +0530

    booking bug fixed

--- schema/bookingModel.js
+++ schema/bookingModel.js
@@ -6,8 +6,8 @@ const bookingSchema = new mongoose.Schema(
         booking_status : {type : Boolean},
         user_id : {type : String},
         turf_id : {type : String},
-        turf_index : {type : Number},
-        time_period : {type : Array},
+        turf_index : {type : Array},
+        time_period : {type : Number},
         booking_price : {type : Number}
     }
 )
