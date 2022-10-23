f4a7172958e0c5214fd3da432b9745cafee89530
Mushthak <100484745+MushthakVP@users.noreply.github.com>
2022-10-23 13:17:37 +0530

    bug v2

--- schema/bookingModel.js
+++ schema/bookingModel.js
@@ -3,12 +3,8 @@ const mongoose = require("mongoose")
 const bookingSchema = new mongoose.Schema(
     {
         booking_date : {type : String},
-        booking_status : {type : Boolean},
-        user_id : {type : String},
         turf_id : {type : String},
-        turf_index : {type : Array},
-        time_period : {type : Number},
-        booking_price : {type : Number}
+        time_slot : {type : Array}
     }
 )
 
